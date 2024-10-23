828a20d8ec839e3e3a8896b5ac2846f9f5605354
MohitBajpai78271 <mohitbajpai997@gmail.com>
2024-10-23 08:11:32 +0530

    Update ApiKeys.swift

--- COP/Model/Utilities/ApiKeys.swift
+++ COP/Model/Utilities/ApiKeys.swift
@@ -9,6 +9,6 @@ import UIKit
 
 struct ApiKeys{
 
-    static let baseURL = "http://93.127.172.217:4000"
-    static let baseURL2 = "http://93.127.172.217:5000"
+    static let baseURL = ""
+    static let baseURL2 = ""
 }
